929662f87e97e82e3cbb3d38871f72281e2f0cdc
iufb <bashirov3ld2@gmail.com>
2025-03-07 16:56:12 +0500

    feat: picker show photos

--- components/AssetsPicker.tsx
+++ components/AssetsPicker.tsx
@@ -2,19 +2,31 @@ import { Typography } from "@/components/ui";
 import { Colors } from "@/constants/Colors";
 import { DeviceWidth, rS, rV } from "@/utils";
 import { useEffect, useState } from "react";
-import { DeviceEventEmitter, Dimensions, Image, Modal, Pressable, StyleSheet, View } from "react-native";
+import { DeviceEventEmitter, Dimensions, FlatList, Image, Modal, Pressable, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Animated, { Easing, ReduceMotion, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 import * as MediaLibrary from 'expo-media-library';
 interface AssetsPickerBase {
-    handlePick: () => void
+    handleSelect: (asset: MediaLibrary.Asset) => void
+}
+const getAssets = async (mediaType: MediaLibrary.MediaTypeValue, save: (assets: MediaLibrary.Asset[]) => void) => {
+    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
+        includeSmartAlbums: true,
+    });
+    let promises: Promise<MediaLibrary.PagedInfo<MediaLibrary.Asset>>[] = []
+    fetchedAlbums.forEach(async (album) => {
+        promises.push(MediaLibrary.getAssetsAsync({ album, mediaType }))
+    })
+    const res = (await Promise.all(promises)).map(page => page.assets)
+    save(res[0])
 }
 export const AssetsPicker = () => {
     const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
     const [visible, setVisible] = useState(false);
     const [saveCb, setSaveCb] = useState<(() => void) | null>(null)
     const [activeTab, setActiveTab] = useState(tabs[0])
+    const [selectedMap, setSelectedMap] = useState<Map<string, MediaLibrary.Asset>>(new Map())
 
     useEffect(() => {
         const listener = DeviceEventEmitter.addListener("openAssetsPicker", (callback: () => void) => {
@@ -24,12 +36,19 @@ export const AssetsPicker = () => {
 
         return () => listener.remove();
     }, []);
+    const handleSelect = (asset: MediaLibrary.Asset) => {
+        setSelectedMap(prev => {
+            const newMap = new Map(prev)
+            newMap.set(asset.id, asset)
+            return newMap
+        })
+    }
 
     return visible && <GestureHandlerRootView><Modal visible={visible} transparent={true} animationType="slide" onRequestClose={() => setVisible(false)}>
         <View style={[styles.container]}>
             <View style={[styles.top]} />
             <Tabs activeTab={activeTab} setActiveTab={(tab) => setActiveTab(tab)} />
-            {activeTab == tabs[0] ? <PhotosView /> : <VideosView />}
+            {activeTab == tabs[0] ? <PhotosView handleSelect={handleSelect} /> : <VideosView handleSelect={handleSelect} />}
         </View>
     </Modal></GestureHandlerRootView>
 
@@ -64,17 +83,7 @@ const Tabs = ({ activeTab, setActiveTab }: TabsProps) => {
     </View>
 
 }
-const getAssets = async (mediaType: MediaLibrary.MediaTypeValue, save: (assets: MediaLibrary.Asset[]) => void) => {
-    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
-        includeSmartAlbums: true,
-    });
-    let promises: Promise<MediaLibrary.PagedInfo<MediaLibrary.Asset>>[] = []
-    fetchedAlbums.forEach(async (album) => {
-        promises.push(MediaLibrary.getAssetsAsync({ album, mediaType }))
-    })
-    const res = (await Promise.all(promises)).map(page => page.assets)
-    save(res[0])
-}
+
 
 
 const PhotosView = ({ }: AssetsPickerBase) => {
@@ -85,11 +94,7 @@ const PhotosView = ({ }: AssetsPickerBase) => {
     console.log(photos, "PHOTOS")
     return <View>
         <Typography variant="h2">Photos</Typography>
-        <View>
-            {photos.map(photo =>
-                <Image key={photo.id} source={{ uri: photo.uri }} width={100} height={100} />
-            )}
-        </View>
+        <AssetsView assets={photos} />
     </View>
 }
 
@@ -99,7 +104,22 @@ const VideosView = ({ }: AssetsPickerBase) => {
     </View>
 
 }
-
+const AssetsView = ({ assets }: { assets: MediaLibrary.Asset[] }) => {
+    return <FlatList
+        data={assets}
+        numColumns={4}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: asset }) =>
+            <AssetItem asset={asset} />
+        }
+    />
+}
+interface AssetItemProps {
+    asset: MediaLibrary.Asset
+}
+const AssetItem = ({ asset }: AssetItemProps) => {
+    return <Image key={asset.id} source={{ uri: asset.uri }} width={DeviceWidth / 4} height={DeviceWidth / 4} />
+}
 const styles = StyleSheet.create({
     container: {
         position: 'relative',
